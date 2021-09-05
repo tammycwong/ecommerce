b3e33d56eb6f35ec73bccd11ec01747873dfb7b3
Tammy <tammycwong@gmail.com>
2021-09-05 00:03:54 -0400

    image folder

--- ecommerce-frontend/src/App.js
+++ ecommerce-frontend/src/App.js
@@ -18,37 +18,62 @@ function App() {
       <main>
         <div class="row center">
           <div class="card">
-            {/* <a href="product.html">
-    <img src="https://3c9sm1yzqy518hwx3f6o4c64-wpengine.netdna-ssl.com/wp-content/uploads/2020/11/yu-xuan-lee-2i5ZX7Fid4M-unsplash-scaled.jpg" alt="Product" />
-  </a>
-          <a href="product.html">
-    <img class="medium" src="https://www.thehouseofsasha.com/uploads/houseofsasha/articles/b9-711823_l.jpg"/>
-  </a>  */}
             <a href="product.html">
-              <img src="https://www.plants.com/images/157646lgp_20201029-1604001560668.webp" />
+              <img src="https://www.plants.com/images/1566418978368_20190821-1566418982051.webp" />
             </a>
-            <div class="card-body">
+            <div className="card-body">
               <a href="product.html">
                 <h2>ZZ plant</h2>
               </a>
-              <div class="rating">
+              <div className="rating">
                 <span>
-                  <i class="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
                 </span>
                 <span>
-                  <i class="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
                 </span>
                 <span>
-                  <i class="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
                 </span>
                 <span>
-                  <i class="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
                 </span>
                 <span>
-                  <i class="fa fa-star-half-o"></i>
+                  <i className="fa fa-star-half-o"></i>
                 </span>
               </div>
-              <div class="price">
+              <div className="price">
+                <p>$120</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="card">
+            <a href="product.html">
+              <img src="https://www.plants.com/images/1566427725771_20190821-1566427728395.webp" />
+            </a>
+            <div className="card-body">
+              <a href="product.html">
+                <h2>Money Tree</h2>
+              </a>
+              <div className="rating">
+                <span>
+                  <i className="fa fa-star"></i>
+                </span>
+                <span>
+                  <i className="fa fa-star"></i>
+                </span>
+                <span>
+                  <i className="fa fa-star"></i>
+                </span>
+                <span>
+                  <i className="fa fa-star"></i>
+                </span>
+                <span>
+                  <i className="fa fa-star"></i>
+                </span>
+              </div>
+              <div className="price">
                 <p>$120</p>
               </div>
             </div>
@@ -56,73 +81,11 @@ function App() {
 
           <div class="card">
             <a href="product.html">
-              <img src="https://www.plants.com/images/157646lgp_20201029-1604001560668.webp" />
+              <img src="https://www.plants.com/images/177111_20210218-1613669870200.webp" />
             </a>
             <div class="card-body">
               <a href="product.html">
-                <h2>ZZ plant</h2>
-              </a>
-              <div class="rating">
-                <span>
-                  <i class="fa fa-star"></i>
-                </span>
-                <span>
-                  <i class="fa fa-star"></i>
-                </span>
-                <span>
-                  <i class="fa fa-star"></i>
-                </span>
-                <span>
-                  <i class="fa fa-star"></i>
-                </span>
-                <span>
-                  <i class="fa fa-star"></i>
-                </span>
-              </div>
-              <div class="price">
-                <p>$120</p>
-              </div>
-            </div>
-          </div>
-
-          <div class="card">
-            <a href="product.html">
-              <img src="https://www.plants.com/images/157646lgp_20201029-1604001560668.webp" />
-            </a>
-            <div class="card-body">
-              <a href="product.html">
-                <h2>ZZ plant</h2>
-              </a>
-              <div class="rating">
-                <span>
-                  <i class="fa fa-star"></i>
-                </span>
-                <span>
-                  <i class="fa fa-star"></i>
-                </span>
-                <span>
-                  <i class="fa fa-star"></i>
-                </span>
-                <span>
-                  <i class="fa fa-star"></i>
-                </span>
-                <span>
-                  <i class="fa fa-star"></i>
-                </span>
-              </div>
-              <div class="price">
-                <p>$120</p>
-              </div>
-            </div>
-          </div>
-
-          <div class="card">
-            <a href="product.html">
-              <img src="https://www.plants.com/images/157646lgp_20201029-1604001560668.webp" />
-            </a>
-            <div class="card-body">
-              <a href="product.html">
-                <h2>ZZ plant</h2>
+                <h2>Aloe Vera Plant</h2>
               </a>
               <div class="rating">
                 <span>
@@ -149,11 +112,42 @@ function App() {
 
           <div class="card">
             <a href="product.html">
+              <img src="https://www.plants.com/images/157639L_20201120-1605903492274.webp" />
+            </a>
+            <div class="card-body">
+              <a href="product.html">
+                <h2>Pothos</h2>
+              </a>
+              <div class="rating">
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+              </div>
+              <div class="price">
+                <p>$120</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="card">
+            <a href="product.html">
               <img src="https://www.plants.com/images/157646lgp_20201029-1604001560668.webp" />
             </a>
             <div class="card-body">
               <a href="product.html">
-                <h2>ZZ plant</h2>
+                <h2>Snake Plant</h2>
               </a>
               <div class="rating">
                 <span>
---
+++ ecommerce-frontend/src/data.js
@@ -0,0 +1,54 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'ZZ plant',
+            category: 'Plants',
+            image: '',
+            price: 120,
+            rating: 5,
+            numReviews: 10,
+            description: 'indoor plant'
+        },
+        {
+            _id: '2',
+            name: 'Snake plant',
+            category: 'Plants',
+            image: '',
+            price: 120,
+            rating: 5,
+            numReviews: 4,
+            description: 'indoor plant'
+        },
+        {
+            _id: '3',
+            name: 'Spider plant',
+            category: 'Plants',
+            image: '',
+            price: 120,
+            rating: 5,
+            numReviews: 10,
+            description: 'indoor plant'
+        },
+        {
+            _id: '4',
+            name: 'Money Tree',
+            category: 'Plants',
+            image: '',
+            price: 20,
+            rating: 5,
+            numReviews: 17,
+            description: 'indoor plant'
+        },
+        {
+            _id: '5',
+            name: 'Pothos',
+            category: 'Plants',
+            image: '',
+            price: 80,
+            rating: 5,
+            numReviews: 10,
+            description: 'indoor plant'
+        },
+    ],
+};
