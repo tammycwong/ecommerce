1ce3341955542f104e92fe7bb6e18ada7117d498
Tammy <tammycwong@gmail.com>
2021-09-06 18:28:30 -0400

    redux for homescreen

--- ecommerce-frontend/src/actions/productActions.js
+++ ecommerce-frontend/src/actions/productActions.js
@@ -1,17 +1,40 @@
-import Axios from 'axios';
-import { PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from '../constants/productConstants';
+import Axios from "axios";
+import {
+  PRODUCT_DETAILS_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_LIST_FAIL,
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS,
+} from "../constants/productConstants";
 
 export const listProducts = () => async (dispatch) => {
-    dispatch({
-        type: PRODUCT_LIST_REQUEST
-    });
-    try {
+  dispatch({
+    type: PRODUCT_LIST_REQUEST,
+  });
+  try {
     //fetches data^
-        const { data } = await Axios.get("/api/products");
-        //sends ajax request to get list of products^
-        dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
-        //dispatching action changes state of redux ; updates homescreen to show products
-    } catch(error) {
-        dispatch({ type: PRODUCT_LIST_FAIL, payload: error.message })
-    }
+    const { data } = await Axios.get("/api/products");
+    //sends ajax request to get list of products^
+    dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
+    //dispatching action changes state of redux ; updates homescreen to show products
+  } catch (error) {
+    dispatch({ type: PRODUCT_LIST_FAIL, payload: error.message });
+  }
+};
+
+export const detailsProduct = (productId) => async (dispatch) => {
+  dispatch({ type: PRODUCT_DETAILS_REQUEST, paylod: productId });
+  try {
+    const { data } = await Axios.get(`/api/products/${productId}`);
+    dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
 };
