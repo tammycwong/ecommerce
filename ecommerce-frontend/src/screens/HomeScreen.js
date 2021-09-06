ff2af0675eb810ad60b03cc898a3efd4e6105a6e
Tammy <tammycwong@gmail.com>
2021-09-05 22:02:23 -0400

    linked backend with frontend

--- ecommerce-frontend/src/screens/HomeScreen.js
+++ ecommerce-frontend/src/screens/HomeScreen.js
@@ -1,12 +1,22 @@
-import React from "react";
-import data from "../data";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Product from "../components/Product";
 
 export default function HomeScreen() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const {data} = await axios.get('/api/products');
+      setProducts(data);
+    };
+    //call function to run it
+    fetchData();
+  }, []);
+
   return (
     <div>
       <div className="row center">
-        {data.products.map((product) => (
+        {products.map((product) => (
           <div key={product.id} className="card">
             <Product key={product.id} product={product} />
           </div>
