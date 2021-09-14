86b651eb87c4c3318f3a092ad3eee908fc70b2b9
Tammy <tammycwong@gmail.com>
2021-09-13 21:59:22 -0400

    need to fix proxy error / backened connection

--- ecommerce-frontend/src/actions/cartActions.js
+++ ecommerce-frontend/src/actions/cartActions.js
@@ -1,5 +1,5 @@
 import Axios from "axios";
-import { CART_ADD_ITEM } from "../constants/cartConstant";
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstant";
 
 export const addToCart = (productId, qty) => async (dispatch, getState) => {
   //defining action always returns async action has 2 parameters, dispatch and getState
@@ -17,3 +17,8 @@ export const addToCart = (productId, qty) => async (dispatch, getState) => {
   });
   localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
 };
+
+export const removeFromCart = (productId) => (dispatch, getState) => {
+  dispatch({type: CART_REMOVE_ITEM, payload: productId});
+  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
+}
--- ecommerce-frontend/src/screens/CartScreen.js
+++ ecommerce-frontend/src/screens/CartScreen.js
@@ -1,7 +1,9 @@
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react/cjs/react.development";
-import { addToCart } from "../actions/cartActions";
+import { addToCart, removeFromCart } from "../actions/cartActions";
+import MessageBox from "../components/MessageBox";
+import { Link } from "react-router-dom";
 
 export default function CartScreen(props) {
   const productId = props.match.params.id;
@@ -10,18 +12,90 @@ export default function CartScreen(props) {
     : 1;
   //location.search returns the value of {qty} in url of addtocarthandler
   //if exist, cast Number in query string
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
   const dispatch = useDispatch();
   useEffect(() => {
     if (productId) {
       dispatch(addToCart(productId, qty));
     }
   }, [dispatch, productId, qty]);
+
+  const removeFromCartHandler = (id) => {
+      dispatch(removeFromCart(id));
+  };
+  const checkoutHandler = () => {
+      props.history.push('/signin?redirect=shipping');
+  }
   return (
-    <div>
-      <h1>Cart Screen</h1>
-      <p>
-        ADD TO CART : PRODUCTID: {productId} Qty: {qty}{" "}
-      </p>
+    <div className="row top">
+      <div className="col-2">
+        <h1>Shopping Cart</h1>
+        {cartItems.length === 0 ? (
+          <MessageBox>
+            Cart is empty.<Link to="/"> Go Shopping</Link>
+          </MessageBox>
+        ) : (
+          <ul>
+            {cartItems.map((item) => (
+              <li key={item.product}>
+                <div className="row">
+                  <div>
+                    <img
+                      src={item.image}
+                      alt={item.name}
+                      className="small"
+                    ></img>
+                  </div>
+                  <div className="min-30">
+                    <Link to={`/product/${item.product}`}>{item.name}</Link>
+                  </div>
+                  <div>
+                    <select
+                      value={item.qty}
+                      onChange={(e) =>
+                        dispatch(
+                          addToCart(item.product, Number(e.target.value))
+                        )
+                      }
+                    >
+                      {[...Array(item.countInStock).keys()].map((x) => (
+                        <option key={x + 1} value={x + 1}>
+                          {x + 1}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                  <div>${item.price}</div>
+                  <div>
+                    <button
+                      type="button"
+                      onClick={() => removeFromCartHandler(item.product)}
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+      <div className="col-1">
+        <div className="card card-body">
+          <ul>
+            <li>
+              <h2>
+                Subtotal ({cartItems.reduce((a, c) => a + c.qty, 0)} items) : $
+                {cartItems.reduce((a, c) => a + c.price * c.qty, 0)}
+              </h2>
+            </li>
+            <button type="button" onClick={checkoutHandler} className="primary block disabled" disabled={cartItems.length === 0}>
+                Proceed to Checkout
+            </button>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
