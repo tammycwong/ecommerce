81905ae5c9166de41560c12e81a326e143dffcb9
Tammy <tammycwong@gmail.com>
2021-09-26 16:44:01 -0400

    refined code in Product component

--- ecommerce-frontend/src/components/Product.js
+++ ecommerce-frontend/src/components/Product.js
@@ -2,22 +2,22 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Rating from "./Rating";
 
-export default function Product(props) {
-  const { product } = props;
+export default function Product({product}) {
+  const {id, name, rating, price, numReviews} = product;
 
   return (
     <div key={product.id} className="card">
-      <Link to={`/product/${product.id}`}>
+      <Link to={`/product/${id}`}>
         <img className="medium" src={product.image} alt={product.name} />
       </Link>
       <div className="card-body">
-        <Link to={`/product/${product.id}`}>
-          <h2>{product.name}</h2>
+        <Link to={`/product/${id}`}>
+          <h2>{name}</h2>
         </Link>
-        <Rating rating={product.rating} numReviews={product.numReviews} />
+        <Rating rating={rating} numReviews={numReviews} />
 
         <div className="price">
-          <p>${product.price}</p>
+          <p>${price}</p>
         </div>
       </div>
     </div>
